d5389aca6d70561adcd91d00d60814b12dd16124
weldoj <weldoj201008@gmail.com>
2025-07-14 02:16:34 -0300

    rubocop -a e rails test feito

--- test/controllers/admin/forms_controller_test.rb
+++ test/controllers/admin/forms_controller_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class Admin::FormsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get admin_forms_url
-    assert_response :redirect
+    assert_response :redirect 
   end
 end
