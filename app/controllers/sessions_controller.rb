d3561494defd0c512ce13a1a043491fc719ec4ec
L-user <lpw.contato@gmail.com>
2025-07-17 19:56:27 -0300

    comentei os puts no sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,23 +9,23 @@ class SessionsController < ApplicationController
     identifier = params[:session][:identifier]
     password = params[:session][:password]
 
-    puts "-> identifier recebido: #{identifier.inspect}"
-    puts "-> password recebido: #{password.inspect}"
+    #puts "-> identifier recebido: #{identifier.inspect}"
+    #puts "-> password recebido: #{password.inspect}"
     
-    puts "-> sessao antes: #{session.to_hash.inspect}"
+    #puts "-> sessao antes: #{session.to_hash.inspect}"
     # tenta encontrar o usuario pelo email
     user = User.find_by(email: identifier) ||
           User.find_by(student_enrolment: identifier)
 
-    puts "-> user encontrado: #{user.inspect}"
+    #puts "-> user encontrado: #{user.inspect}"
     
     # Verifica se a senha é a mesma
     valid = user && user.hash_password == password
-    puts "-> senha valida? #{valid}"
+    #puts "-> senha valida? #{valid}"
 
     if valid
       session[:user_id] = user.id
-      puts "->session[:user_id] agr: #{session[:user_id]}"
+      #puts "->session[:user_id] agr: #{session[:user_id]}"
       flash[:sucess] = "Login realizado com sucesso !" # Faz login com sucesso
       redirect_to root_path #VEFICAR SE ESTA ENCAMINHANDO PRO LUGAR CERTO
     else
