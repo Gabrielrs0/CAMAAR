4c8db7c9415cbabd78875588585d1fc4ee683f38
Pedro Ximenes <pedroximenes76@gmail.com>
2025-07-18 16:01:06 -0300

    Estrutura incompleta da controller

---
+++ app/controllers/api/v1/templates_controller.rb
@@ -0,0 +1,137 @@
+class Api::V1::TemplatesController < ApplicationController
+  before_action :set_template
+
+  def index
+    templates = Template.all
+    render json: templates, status: :ok
+  end
+
+  def show
+    # template = Template.find(params[:id])
+    render json: @template
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Template com ID '#{params[:id]}' não encontrado" }, status: :not_found
+  end
+
+  private
+
+  def set_template
+    @template = Template.find(params[:id])
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+
+  def create
+    template = Template.new(template_params)
+    template.save!
+    render json: template, status: :created
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
+  private
+
+  def template_params
+    params.expect(template: %i[title])
+  end
+end
+
+
+
+
+
+
+# # frozen_string_literal: true
+
+# # Adicione um comentário de documentação para a classe TemplatesController.
+# class TemplatesController < ApplicationController
+#   before_action :set_template, only: %i[show edit update destroy]
+
+#   # GET /templates or /templates.json
+#   def index
+#     @templates = Template.all
+#   end
+
+#   # GET /templates/1 or /templates/1.json
+#   def show; end
+
+#   # GET /templates/new
+#   def new
+#     @template = Template.new
+#   end
+
+#   # GET /templates/1/edit
+#   def edit; end
+
+#   # POST /templates or /templates.json
+#   def create
+#     @template = Template.new(template_params)
+
+#     respond_to do |format|
+#       if @template.save
+#         format.html { redirect_to @template, notice: 'Template was successfully created.' }
+#         format.json { render :show, status: :created, location: @template }
+#       else
+#         format.html { render :new, status: :unprocessable_entity }
+#         format.json { render json: @template.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+
+#   # PATCH/PUT /templates/1 or /templates/1.json
+#   def update
+#     respond_to do |format|
+#       if @template.update(template_params)
+#         format.html { redirect_to @template, notice: 'Template was successfully updated.' }
+#         format.json { render :show, status: :ok, location: @template }
+#       else
+#         format.html { render :edit, status: :unprocessable_entity }
+#         format.json { render json: @template.errors, status: :unprocessable_entity }
+#       end
+#     end
+#   end
+
+#   # DELETE /templates/1 or /templates/1.json
+#   def destroy
+#     @template.destroy!
+
+#     respond_to do |format|
+#       format.html { redirect_to templates_path, status: :see_other, notice: 'Template was successfully destroyed.' }
+#       format.json { head :no_content }
+#     end
+#   end
+
+#   private
+
+#   # Use callbacks to share common setup or constraints between actions.
+#   def set_template
+#     @template = Template.find(params.expect(:id))
+#   end
+
+#   # Only allow a list of trusted parameters through.
+#   def template_params
+#     params.expect(template: %i[titulo publico_alvo criado_por_id])
+#   end
+# end
