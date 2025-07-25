bfe70b4e132e7636c0ecda64894c588e8ea987cd
Pedro Ximenes <pedroximenes76@gmail.com>
2025-07-24 22:37:37 -0300

    Merge Weldo2

--- test/controllers/admin/forms_controller_test.rb
+++ test/controllers/admin/forms_controller_test.rb
@@ -2,7 +2,12 @@ require "test_helper"
 
 class Admin::FormsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+<<<<<<< HEAD
     get admin_forms_url
     assert_response :redirect
+=======
+    get admin_forms_index_url
+    assert_response :success
+>>>>>>> fa8b901 (merge parte weldo)
   end
 end
