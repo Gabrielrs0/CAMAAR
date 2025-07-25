15b2449f27fe9d16ff09b7fb81dc3c83fbf54778
Pedro Ximenes <pedroximenes76@gmail.com>
2025-07-24 22:37:37 -0300

    Merge Weldo2

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,11 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+<<<<<<< HEAD
     get root_url
+=======
+    get home_index_url
+>>>>>>> fa8b901 (merge parte weldo)
     assert_response :success
   end
 end
